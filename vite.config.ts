6fcb1b9b9651d11a07ac00ce894f6bb6f0cbb634
playbooks <eric@erichubbell.com>
2025-07-18 17:45:45 -0600

    config update

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,13 @@ import { runSize } from './src';
 import path from 'path';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import { defineConfig } from 'vite';
+import { runYalc } from 'vite-plugin-yalc';
 
 export default defineConfig(({ mode }) => {
 	return {
 		base: './',
 		build: {
+			ssr: true,
 			sourcemap: mode === 'development',
 			lib: {
 				entry: path.resolve(__dirname, 'src/index.ts'),
@@ -30,7 +32,7 @@ export default defineConfig(({ mode }) => {
 				plugins: [peerDepsExternal()],
 			},
 		},
-		plugins: [runSize('dist')],
+		plugins: [runYalc(), runSize('dist')],
 		resolve: {
 			alias: {
 				src: path.resolve(__dirname, '/src'),
