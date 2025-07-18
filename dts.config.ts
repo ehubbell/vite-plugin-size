9a089dd5375ad41b730ac4feeb0b7b82d5450cf5
playbooks <eric@erichubbell.com>
2025-07-18 09:22:30 -0600

    script updates

--- dts.config.ts
+++ dts.config.ts
@@ -1,11 +1,11 @@
 const config = {
-  entries: [
-    {
-      filePath: "./src/index.ts",
-      outFile: "./dist/index.d.ts",
-      noCheck: false,
-    },
-  ],
+	entries: [
+		{
+			filePath: './src/index.ts',
+			outFile: './dist/index.d.ts',
+			noCheck: false,
+		},
+	],
 };
 
 module.exports = config;
